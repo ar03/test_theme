8d812e5995b4f5056d3e9d8d32f51ce2b6756471
Geralt of Rivia <alex@AlexH.local>
2019-11-06 14:00:45 -0600

    initialize repo

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+
+function test_theme_files() {
+  wp_enqueue_script('main_test_js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
+  wp_enqueue_style('custom_google_fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+  wp_enqueue_style('font_awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+  wp_enqueue_style('test_theme_main_styles', get_stylesheet_uri());
+}
+
+add_action('wp_enqueue_scripts', 'test_theme_files');
+?>
